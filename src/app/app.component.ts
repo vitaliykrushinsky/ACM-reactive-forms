e786938755c793c4434b2ef572d26d3644fe2d43
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2017-04-20 11:22:09 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    template: `
+        <div class="container">
+          <my-signup></my-signup>
+        </div>
+    `
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customers/customer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    ReactiveFormsModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    CustomerComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
